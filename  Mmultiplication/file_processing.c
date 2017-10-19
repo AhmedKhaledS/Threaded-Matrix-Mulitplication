1e9728c8c09e1bc78208f6b47c105203f8f160ee
AhmedKhaledS <ahmedkhaled1123300@gmail.com>
2017-10-19 09:58:06 +0000

    added working files

---
+++  Mmultiplication/file_processing.c
@@ -0,0 +1,11 @@
+#include "file_processing.h"
+
+
+void read_matrices(char *m1_file_name, char *m2_file_name)
+{
+
+}
+void write_output(char *m_file_name)
+{
+
+}
---
+++  Mmultiplication/file_processing.h
@@ -0,0 +1,7 @@
+#ifndef FILE_PROCESSING_H_INCLUDED
+#define FILE_PROCESSING_H_INCLUDED
+
+void read_matrices(char *m1_file_name, char *m2_file_name);
+void write_output(char *m_file_name);
+
+#endif // FILE_PROCESSING_H_INCLUDED
