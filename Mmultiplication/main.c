c7a7618eb7880fd5968ca8df41e3426b6a629c92
AhmedKhaledS <ahmedkhaled1123300@gmail.com>
2017-10-19 15:35:20 +0000

    switched to CLion and changed dependencies

--- Mmultiplication/main.c
+++ Mmultiplication/main.c
@@ -1,49 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "matrices_data.h"
-#include "naive_mmultiplication.h"
 #include "file_processing.h"
 #include "matrix_util.h"
 #include "multiply.h"
 
-/*struct m_data* create_mdata(unsigned int row1, unsigned int col1,
-            unsigned int row2, unsigned int col2)
-{
-    struct m_data *matrix_data = (struct m_data *)malloc(sizeof(struct m_data));
-    matrix_data->arow_size = row1;
-    matrix_data->brow_size = row2;
-    matrix_data->acol_size = col1;
-    matrix_data->bcol_size = col2;
-    matrix_data->matrix_a = (long long **)malloc(row1 * sizeof(long long *));
-    matrix_data->matrix_b = (long long **)malloc(row2 * sizeof(long long *));
-    matrix_data->matrix_o = (long long **)malloc(row1 * sizeof(long long *));
-    if (matrix_data->matrix_a == NULL || matrix_data->matrix_b == NULL)
-        fprintf(stderr, "Error while allocating memory for matrices\n");
-    for (int i = 0; i < row1; i++)
-        matrix_data->matrix_a[i] = (long long *) malloc((size_t)col1 * sizeof(long long));
-    for (int i = 0; i < row2; i++)
-        matrix_data->matrix_b[i] = (long long *) malloc((size_t)col2 * sizeof(long long));
-    for (int i = 0; i < row1; i++)
-        matrix_data->matrix_o[i] = (long long *) malloc((size_t)col2 * sizeof(long long));
-    return matrix_data;
-}
 
-void multiply_naive(struct m_data *data)
-{
-    for (int i = 0; i < data->arow_size; i++)
-    {
-        for (int j = 0; j < data->bcol_size; j++)
-        {
-            data->matrix_o[i][j] = 0;
-            for (int k = 0; k < data->acol_size; k++)
-            {
-                data->matrix_o[i][j] += (data->matrix_a[i][k]
-                                    * data->matrix_b[k][j]);
-            }
-        }
-    }
-    return;
-}*/
 
 int main(int argc, char* argv[])
 {
@@ -59,12 +21,12 @@ int main(int argc, char* argv[])
             for (int j = 0; j < 3; j++)
                 x->matrix_a[i][j] = i+j, x->matrix_b[i][j] = i+j;
         multiply_naive(x);
-        /*for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
-                printf("%I64d ", *(*(x->matrix_o+i)+j));
+                printf("%d ", *(*(x->matrix_o+i)+j));
             printf("\n");
-        }*/
+        }
     }
     else if (argc == 4)
     {
